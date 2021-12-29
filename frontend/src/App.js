fb57be0ede3e27a994c52d6a74db5436e06c1ceb
Timothy <timothy.prefol@gmail.com>
2021-12-29 21:13:01 +0100

    Btn gradient

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,25 @@ import styled from "styled-components";
 
 
 const Button = styled.button`
-  text-transform: capitalize;
-  border: none;
-  background: black;
-  color: white;
-  font-weight: 700;
-  font-size: 1em;
-  padding: 0.25em 1em;
-  cursor: pointer;
-  height: 40px;
+    border: none;
+    height: 40px;
+    font-weight: 700;
+    font-size: 1em;
+    background-image: ${props => props.gradient};
+    text-align: center;
+    text-transform: uppercase;
+    transition: 0.5s;
+    background-size: 200% auto;
+    color: white;            
+    box-shadow: 0 0 20px #eee;
+    display: block;
+    :hover {
+        background-position: right center; /* change the direction of the change here */
+        color: #fff;
+        text-decoration: none;
+    }
+      
+
 `;
 
 const ButtonContainer = styled.div`
@@ -34,6 +44,7 @@ function App() {
         <>
             <ButtonContainer>
                     <Button
+                        gradient ={selectedProvider === spotify ? 'linear-gradient(to right, #1DB954 0%, #ff0000  51%, #ff0000  100%)' : 'linear-gradient(to right, #ff0000 0%, #1DB954  51%, #1DB954  100%)'}
                         onClick={() => handleClick(selectedProvider === spotify ? youtube : spotify)}>{selectedProvider === spotify ? youtube : spotify}
                     </Button>
             </ButtonContainer>
--- frontend/src/components/youtube/YoutubeButton.js
+++ frontend/src/components/youtube/YoutubeButton.js
@@ -1,15 +1,31 @@
 import React, {Fragment} from 'react';
 import styled from 'styled-components';
 
+
+
 const Button = styled.button`
-  background: #ff0000;
-  color: white;
-  font-size: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid #ff0000;
-  cursor: pointer;
-  height: 40px;
+    border: none;
+    background-image: linear-gradient(to right, #ee0979 0%, #ff6a00 51%, #ee0979 100%);
+    padding: 0.25em 1em;
+    text-align: center;
+    text-transform: uppercase;
+    transition: 0.5s;
+    background-size: 200% auto;
+    color: white;            
+    box-shadow: 0 0 20px #eee;
+    display: block;
+    :hover {
+        background-position: right center; /* change the direction of the change here */
+        color: #fff;
+        text-decoration: none;
+    }
+           
 `;
+//  .btn-grad:hover {
+//             background-position: right center; /* change the direction of the change here */
+//             color: #fff;
+//             text-decoration: none;
+//           }
 
 function YoutubeButton(props) {
 
