ad55167859a10c6d113bdd0edc970db395ba21a7
Timothy <timothy.prefol@gmail.com>
2022-01-15 17:56:31 +0100

    Update cors

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, '/../frontend/build')));
 
 const cors = require("cors");
 const corsOptions = {
-    origin: 'http://localhost:5000',
+    origin: '*',
     credentials: true,
     optionSuccessStatus: 200,
     exposedHeaders: ['Content-Disposition']
